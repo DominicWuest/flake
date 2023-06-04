075dce5bdc6017fbc7beaaee6bd57732b92360c7
DominicWuest <dominic.wuest@hotmail.com>
2023-06-04 13:43:47 +0200

    Added tmux config

--- home.nix
+++ home.nix
@@ -54,8 +54,6 @@
       recursive = true;
     };
 
-    # tmux
-    
     # vim
 
     # zsh
@@ -69,9 +67,39 @@
 
   };
 
-  programs.go.enable = true;
+  programs.tmux = {
+    enable = true;
+    extraConfig = builtins.readFile ( ./dotfiles/tmux );
+    plugins = with pkgs; [
+      # Resurrect plugin
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+        set -g @resurrect-strategy-vim 'session'
+        set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+
+      # Continuum plugin
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+        set -g @continuum-restore 'on'
+        set -g @continuum-save-interval '5'
+        '';
+      }
+
+      # Gruvbox plugin
+      {
+        plugin = tmuxPlugins.gruvbox;
+        extraConfig = "set -g @tmux-gruvbox 'dark'";
+      }
+    ];
+  };
 
   xresources.extraConfig = builtins.readFile ( ./dotfiles/Xresources );
+
+  programs.go.enable = true;
 
   home.sessionVariables = {
     EDITOR = "vim";
