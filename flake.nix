3b0dbd9be5f9441a4129eb5da41da9fb5580cc14
DominicWuest <dominic.wuest@hotmail.com>
2023-06-04 08:34:15 +0200

    Made flake glue everything together and added home-manager

--- flake.nix
+++ flake.nix
@@ -1,11 +1,41 @@
 {
-  description = "A very basic flake";
+  description = "My NixOS config";
 
-  outputs = { self, nixpkgs }: {
+  inputs = { 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, home-manager }:
+  let
+    user = "dominic";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    lib = nixpkgs.lib;
+  in
+  {
+    nixosConfigurations = {
+      # Laptop
+      onion = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${user} = {
+              imports = [ ./home.nix ];
+            };
+          }
+        ];
+      };
+    };
   };
 }
---
+++ home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "dominic";
+  home.homeDirectory = "/home/dominic";
+
+  home.stateVersion = "23.05";
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    (pkgs.writeShellScriptBin "my-hello" ''
+      echo "Hello, ${config.home.username}!"
+    '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    VISUAL = "vim";
+  };
+
+  programs.home-manager.enable = true;
+}
