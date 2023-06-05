717cc2e69f59a89aa15435910504ec50cb3a7da1
DominicWuest <dominic.wuest@hotmail.com>
2023-06-05 10:09:44 +0200

    Added rules for creating needed go directories

--- home.nix
+++ home.nix
@@ -127,6 +127,12 @@ in
   xresources.extraConfig = builtins.readFile ( ./dotfiles/Xresources );
 
   programs.go.enable = true;
+  # Add needed go directories
+  systemd.user.tmpfiles.rules = [
+    "d /home/${user}/go/src 0755 ${user} users"
+    "d /home/${user}/go/bin 0755 ${user} users"
+    "d /home/${user}/go/pkg 0755 ${user} users"
+  ];
 
   programs.zsh = {
 		enable = true;
