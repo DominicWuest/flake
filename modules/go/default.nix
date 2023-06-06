e9901742d874567aae21b6810c1f198a99a5f51a
DominicWuest <dominic.wuest@hotmail.com>
2023-06-06 21:18:34 +0200

    Added more go packages

--- modules/go/default.nix
+++ modules/go/default.nix
@@ -1,10 +1,30 @@
 { pkgs, user, ... }:
 
+let
+  pkgsite = with pkgs; buildGoModule
+    {
+      name = "pkgsite";
+      src = fetchFromGitHub {
+        owner = "golang";
+        repo = "pkgsite";
+        rev = "8e5e504098ab1a2d307a1300a817660a892604b1";
+        sha256 = "sha256-UVYfEjwhqVEdFsI2hcxHa901c6Kf682FzNxBLWa1WZU=";
+      };
+      vendorHash = "sha256-HWeKFPqPeIF6hivuTHneZ7O5J/t1qa9yWGunhGqE4/k=";
+      checkPhase = ''
+        export GOWORK=off
+      '';
+    };
+in
 {
   home.packages = with pkgs;
     [
       gopls # language server
       delve # debugger
+
+      # Third party dev tools
+      cobra-cli
+      pkgsite
     ];
 
   programs.go.enable = true;
