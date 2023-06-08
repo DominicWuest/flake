468ac466961ff43d6ae461ced3d30674a250f3ae
DominicWuest <dominic.wuest@hotmail.com>
2023-06-08 02:45:27 +0200

    Added htop to system packages

--- hosts/onion/configuration.nix
+++ hosts/onion/configuration.nix
@@ -87,7 +87,8 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
+    htop
     wget
     git
   ];
